1f7c87bab14edb1d95afa275507c3a62a627ba7d
Luis Pujols <luispujols@hotmail.com>
2022-11-18 17:27:35 -0500

    Added octokit webhook dotnet tools and configured webhook secret

--- branch-protector/Controllers/RepositoriesController.cs
+++ branch-protector/Controllers/RepositoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Octokit.Webhooks;
 
 namespace branch_protector.Controllers
 {
@@ -21,19 +22,6 @@ namespace branch_protector.Controllers
         public IEnumerable<string> GetRepositories()
         {
             return new string[] { "octo-guard", "octo-defender" };
-        }
-
-        /// <summary>
-        /// Receives GitHub Events related to the Organization Repositories 
-        /// </summary>
-        /// <response code="200">Processed GitHub Event Successfully</response>
-        /// <response code="404">Could not process GitHub Event</response>
-        [HttpPost("webhook")]
-        public async Task<IActionResult> Webhook()
-        {
-            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
-            _logger.LogDebug($"--- Received GitHub Event ---- {json}");
-            return Ok();
         }
     }
 }
---
+++ branch-protector/GitHubWebhookEventProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using branch_protector.Controllers;
+using Microsoft.Extensions.Logging;
+using Octokit.Webhooks;
+using Octokit.Webhooks.Events;
+using Octokit.Webhooks.Events.InstallationRepositories;
+using Octokit.Webhooks.Events.Repository;
+
+namespace branch_protector
+{
+    /// <summary>
+    /// Receives GitHub Events related to the Organization Repositories 
+    /// </summary>
+    public class GitHubWebhookEventProcessor : WebhookEventProcessor
+    {
+        private readonly ILogger<GitHubWebhookEventProcessor> _logger;
+
+        public GitHubWebhookEventProcessor(ILogger<GitHubWebhookEventProcessor> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task ProcessRepositoryWebhookAsync(WebhookHeaders headers, RepositoryEvent repositoryEvent, RepositoryAction action)
+        {
+
+            _logger.LogDebug($"--- Received GitHub Repository Event ---- {repositoryEvent}");
+            _logger.LogDebug($"--- GitHub Repository Event Action ---- {action}");
+
+            switch(repositoryEvent.Action)
+            {
+                case "created":
+                    _logger.LogDebug($"--- Received Supported Action ---- {action}");
+                    break;
+                default:
+                    _logger.LogDebug($"--- Received Unsupported Action {repositoryEvent.Action}");
+                    break;
+            }
+
+            return base.ProcessRepositoryWebhookAsync(headers, repositoryEvent, action);
+        }
+    }
+}
+
