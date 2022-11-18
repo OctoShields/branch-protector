353e4a19e84c5c5210fd9d7614c8753a0f1a5ba3
Luis Pujols <luispujols@hotmail.com>
2022-11-17 21:25:56 -0500

    Added initial project structure
    Project will be developed with ASP.NET Core

---
+++ branch-protector/Controllers/RepositoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace branch_protector.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RepositoriesController : ControllerBase
+    {
+        private readonly ILogger<RepositoriesController> _logger;
+
+        public RepositoriesController(ILogger<RepositoriesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public IEnumerable<string> GetRepositories()
+        {
+            return new string[] { "octo-guard", "octo-defender" };
+        }
+    }
+}
+
